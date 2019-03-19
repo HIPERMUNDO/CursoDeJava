92afab0174941b2906eb4cae331ff96e2fde0e97
HIPERMUNDO <48701913+HIPERMUNDO@users.noreply.github.com>
2019-03-18 23:01:29 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package tutojava;//o nome da pasta que esse arquivo se encontra ou a pasta do projeto 
+
+public class HelloWorld{
+
+ public static void main(String[] args){
+
+  System.outros.println("Hello World !");
+
+ }
+
+}
